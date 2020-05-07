10d32c52959947edcf845b7a54fb393842bb3cb2
Osong Agberndifor <agberndifor7@gmail.com>
2020-05-07 09:55:07 +0100

    Add method to creat new object for each string

--- lib/load_file.rb
+++ lib/load_file.rb
@@ -4,5 +4,15 @@ class LoadFile
   def initialize
     @file = file
     @file_content = file_content
+
+    file_content = get_content(file)
+  end
+
+  def get_content(file)
+    contents = ''
+    File.open(file, 'r') { |strings| contents = strings.readlines.map(&:chomp) }
+    scanned_content = contents.collect { |string| scanned = StringScanner.new(string) }
+  
+    scanned_content
   end
 end
