a8b3ed75bb2b65d8fec1002b9d0cd970db6767b9
Osong Agberndifor <agberndifor7@gmail.com>
2020-05-08 10:13:39 +0100

    Add more test and colorize to make output a little better

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,15 @@
 require_relative '../lib/load_file.rb'
 require_relative '../lib/linter.rb'
+require 'colorize'
 
 include Linter # rubocop: disable Style/MixinUsage
 
 file = ARGV.shift
 new_file = LoadFile.new(file)
 puts
-puts 'Checking File...'
+puts 'Checking File...'.cyan
 puts
 
 check_line_spacing(new_file.file_content)
 puts
-puts 'All Done!'
+puts 'All Done!'.cyan
