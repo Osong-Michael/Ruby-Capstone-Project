5aae8e22a24c78ebc3f2b4a5f78b58a04b876611
Osong Agberndifor <agberndifor7@gmail.com>
2020-05-11 12:49:14 +0100

    Add linter for indentation

--- bin/main.rb
+++ bin/main.rb
@@ -2,15 +2,18 @@
 
 require_relative '../lib/load_file.rb'
 require_relative '../lib/line_space.rb'
+require_relative '../lib/indent_check.rb'
 require 'colorize'
 
 file = ARGV.shift
 new_file = LoadFile.new(file)
 space_check = LineSpace.new
+indent_check = IndentationCheck.new
 puts
 puts 'Checking File...'.cyan
 puts
 
 space_check.check_line_spacing(new_file.file_content)
+indent_check.indentation_check(new_file.file_content)
 puts
 puts 'All Done!'.cyan
--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../lib/load_file.rb'
 require_relative '../lib/line_space.rb'
+require_relative '../lib/indent_check.rb'
 
 RSpec.describe LineSpace do
   let(:file) { 'spec/test_spec.css' }
   let(:new_file) { LoadFile.new(file) }
   let(:space_check) { LineSpace.new }
+  let(:indent_check) { IndentationCheck.new }
 
   describe "#line_spacing_after \',\'" do
     it 'Returns an error for wrong spacing between selectors' do
@@ -27,6 +29,14 @@ RSpec.describe LineSpace do
       expect do
         space_check.line_spacing_before(6, new_file.file_content[5], '{')
       end.to output("6: x Expected single space before \"{\"\n").to_stdout
+    end
+  end
+
+  describe '#indentation_error?' do
+    it 'Returns an error message for wrong indentation' do
+      expect do
+        indent_check.indentation_error?(new_file.file_content[7], 8)
+      end.to output("8: x Expected 2 space indentation\n").to_stdout
     end
   end
 end
